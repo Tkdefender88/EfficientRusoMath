64d69b249c53f9ba1748edbb20b163f321017f65
TKDefender <tkdefender88@gmail.com>
2019-03-05 14:30:05 -0700

    Added multiplication function, and TVal to Alarusse files

--- AlaRusse.cpp
+++ AlaRusse.cpp
@@ -1,11 +1,36 @@
 /**
  * S19 CSCI 332 Design and Analysis of Algorithms
- * 
+ *
  * Project-1 Efficient Russian Multiplication
- * 
+ *
  * A La Russe Multiplication Algorithm implementation
- * 
+ *
  * Phillip J. Curtiss, Assistant Professor
  * pcurtiss@mtech.edu, 406-496-4807
  * Department of Computer Science, Montana Tech
  */
+
+#ifndef ALA_RUSSE_CPP
+#define ALA_RUSSE_CPP
+
+#include "AlaRusse.h"
+int shiftCounts = 0;
+
+TVal Multiply(TVal nVal, TVal mVal) {
+    // check if the value has overflowed an int
+    TVal sum;
+    sum.iVal = 0;
+    for (unsigned long long n = nVal.ullVal, m = mVal.ullVal; n >= 1;
+         n >>= 1, m <<= 1) {
+        // check if n is odd
+        if (n % 2) {
+            sum.ullVal += m;
+            if (n == 1) {
+                return sum;
+            }
+        }
+        shiftCounts += 2;
+    }
+    return sum;
+}
+#endif
--- AlaRusse.h
+++ AlaRusse.h
@@ -1,11 +1,53 @@
 /**
  * S19 CSCI 332 Design and Analysis of Algorithms
- * 
+ *
  * Project-1 Efficient Russian Multiplication
- * 
+ *
  * A La Russe Multiplication Algorithm class
- * 
+ *
  * Phillip J. Curtiss, Assistant Professor
  * pcurtiss@mtech.edu, 406-496-4807
  * Department of Computer Science, Montana Tech
  */
+
+#ifndef ALA_RUSSE_H
+#define ALA_RUSSE_H
+
+// used to hold values of type int or values of unsigned long long int
+// but not both - you may check the value - if the TVal.iVal < 0 then
+// access the value in TVal.ullVal for the correct value
+using TVal = union val {
+    // 16 bits; max_int 2147483647 2^31 - 1
+    int iVal = -1;
+    // 64 bits; max_ullint 18446744073709551615 2^64-1
+    unsigned long long ullVal;
+    bool operator<(const val& t) const {
+        // Check to see if the iVal has overflowed
+        if (this->iVal & 0x8000 || t.iVal & 0x8000) {
+            return this->ullVal < t.ullVal;
+        }
+        return this->iVal < t.iVal;
+    }
+
+    bool operator>(const val& t) const {
+        // Check to see if the iVal has overflowed
+        if ((this->iVal & 0x8000) || (t.iVal & 0x8000)) {
+            return this->ullVal > t.ullVal;
+        }
+        return this->iVal > t.iVal;
+    }
+
+    val operator=(const val& t) {
+        this->ullVal = t.ullVal;
+        this->iVal = t.iVal;
+        return *this;
+    }
+};
+
+// A value to count the number of bit shifts beformed by Multiply
+extern int shiftCounts;
+
+// Takes two values of TVal and performs russian peasant multiplication. It then
+// returns a TVal with the result.
+TVal Multiply(const TVal nVal, const TVal mVal);
+#endif
